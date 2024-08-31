8d088ec9988d54be8839e72a9481342cbd6ef1a2
Daniel Barker <phuebu@googlemail.com>
2024-08-31 08:50:21 +0100

    add wasm-opt pass

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@
 
 cargo build --lib --release --target wasm32-unknown-unknown
 wasm-bindgen --target web --no-typescript --out-dir static target/wasm32-unknown-unknown/release/wasm_lbm.wasm
+wasm-opt static/wasm_lbm_bg.wasm -o static/wasm_lbm_bg.wasm -O4
